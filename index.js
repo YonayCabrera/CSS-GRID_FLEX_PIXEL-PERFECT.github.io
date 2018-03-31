702680adf29db1e06ac89e314a7d3bef6b633a43
unknown <yonaycl@gmail.com>
2018-03-31 16:25:22 +0100

    ciclist and blogs of image

---
+++ index.js
@@ -0,0 +1,9 @@
+function initMap() {
+    let map;
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 27.986870, lng: -15.393679},
+        zoom: 17
+    });
+}
+
+
